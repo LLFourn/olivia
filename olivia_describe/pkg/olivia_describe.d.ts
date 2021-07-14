520935fa117abc6f91b1d9d66a2bbc671d4b427d
LLFourn <lloyd.fourn@gmail.com>
2021-07-14 20:00:33 +1000

    Add wasm bindings from olivia_describe

---
+++ olivia_describe/pkg/olivia_describe.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} path
+* @returns {string | undefined}
+*/
+export function path_short(path: string): string | undefined;
+/**
+* @param {string} path
+* @returns {string | undefined}
+*/
+export function path_long(path: string): string | undefined;
+/**
+* @param {string} event_id
+* @returns {string | undefined}
+*/
+export function event_short(event_id: string): string | undefined;
---
+++ olivia_describe/pkg/olivia_describe.js
@@ -0,0 +1,2 @@
+import * as wasm from "./olivia_describe_bg.wasm";
+export * from "./olivia_describe_bg.js";
