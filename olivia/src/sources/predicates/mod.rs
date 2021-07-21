3d81fc10513c0d159d0912c9b8ae2bc2b2232638
LLFourn <lloyd.fourn@gmail.com>
2021-07-21 11:41:31 +1000

    Make outcome filter config make sense

--- olivia/src/sources/predicates/mod.rs
+++ olivia/src/sources/predicates/mod.rs
@@ -4,16 +4,23 @@ pub use eq::*;
 use olivia_core::EventId;
 
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+#[serde(untagged)]
 pub enum OutcomeFilter {
-    All,
-    Chosen(Vec<u64>),
+    Pattern(Pattern),
+    Indexes(Vec<u64>),
 }
 
 impl OutcomeFilter {
     pub fn outcomes_for(&self, id: &EventId) -> Vec<u64> {
         match self {
-            OutcomeFilter::All => (0..id.n_outcomes()).collect::<Vec<_>>(),
-            OutcomeFilter::Chosen(chosen) => chosen.clone(),
+            OutcomeFilter::Pattern(Pattern::All) => (0..id.n_outcomes()).collect::<Vec<_>>(),
+            OutcomeFilter::Indexes(chosen) => chosen.clone(),
         }
     }
+}
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub enum Pattern {
+    #[serde(rename = "*")]
+    All,
 }
