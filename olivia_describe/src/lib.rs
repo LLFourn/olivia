025cb6ad31b85d3150571b902e3502a16cab88ad
LLFourn <lloyd.fourn@gmail.com>
2021-09-29 14:28:04 +1000

    s/Sports and Esports/Sports/

--- olivia_describe/src/lib.rs
+++ olivia_describe/src/lib.rs
@@ -80,7 +80,7 @@ impl DateTime {
 pub fn path_short(path: PathRef<'_>) -> Option<String> {
     let segments = path.segments().collect::<Vec<_>>();
     let desc = match &segments[..] {
-        ["s"] => format!("sports and Esports competitions"),
+        ["s"] => format!("sport and Esport competitions"),
         ["s", competition, "match", tail @ ..] => match tail {
             [] => format!("{} matches", lookup_competition(competition)),
             [date] => format!(
@@ -360,7 +360,7 @@ pub fn long_path_name_str(path: &str) -> Option<String> {
     let path = Path::from_str(path).ok()?;
     let segments = path.as_path_ref().segments().collect::<Vec<_>>();
     Some(match &segments[..] {
-        ["s"] => "Sports and Esports".into(),
+        ["s"] => "Sport".into(),
         ["s", "EPL"] => "English Premier League".into(),
         ["s", competition, "match", _date, teams] => {
             let (left, right) = {
